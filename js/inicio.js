0c067abf3ef9585e83376a3671af0b2f25b88a3f
BastiCan <9bastianzcaniviloaraya@gmail.com>
2026-08-23 13:58:43 -0400

    Feat: linking with the other files

--- js/inicio.js
+++ js/inicio.js
@@ -2,3 +2,18 @@ const registro = document.getElementById("registro");
 registro.addEventListener("click", function() {
     window.location.href = "../html/registro.html";
 });
+
+const avistamiento = document.getElementById("avistamiento");
+avistamiento.addEventListener("click", function() {
+    window.location.href = "../html/avistamiento.html";
+})
+
+const listado = document.getElementById("listado");
+listado.addEventListener("click", function() {
+    window.location.href = "../html/listado.html";
+})
+
+const estadistica = document.getElementById("estadistica");
+estadistica.addEventListener("click", function() {
+    window.location.href = "../html/estadisticas.html";
+})

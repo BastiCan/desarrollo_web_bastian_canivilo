3530abe6ea56614520f5af7ddba550f7921bb100
BastiCan <9bastianzcaniviloaraya@gmail.com>
2026-09-05 13:36:38 -0400

    Feat: add filters to the table

--- js/listado.js
+++ js/listado.js
@@ -1,5 +1,14 @@
 const bdAvistamientos = [
-    {tipo: "lol", nombre: "paloma", lugar: "mi casa", fecha: "10/10/2001", hora: "10:10"}
+    { tipo: "Ave rapaz", nombre: "Cóndor Andino", lugar: "Cajón del Maipo", fecha: "01/09/2026", hora: "10:30" },
+    { tipo: "Ave marina", nombre: "Pingüino de Humboldt", lugar: "Reserva Nacional Pingüino de Humboldt", fecha: "28/08/2026", hora: "09:15" },
+    { tipo: "Pájaro carpintero", nombre: "Carpintero Negro", lugar: "Parque Nacional Conguillío", fecha: "03/09/2026", hora: "14:20" },
+    { tipo: "Ave acuática", nombre: "Cisne de Cuello Negro", lugar: "Santuario Carlos Anwandter", fecha: "02/09/2026", hora: "11:45" },
+    { tipo: "Ave zancuda", nombre: "Flamenco Chileno", lugar: "Salar de Atacama", fecha: "04/09/2026", hora: "17:30" },
+    { tipo: "Rapaz nocturna", nombre: "Tucúquere", lugar: "Reserva Nacional Altos de Lircay", fecha: "01/09/2026", hora: "21:15" },
+    { tipo: "Paseriforme", nombre: "Loica", lugar: "Parque Metropolitano de Santiago", fecha: "05/09/2026", hora: "08:30" },
+    { tipo: "Ave marina", nombre: "Pelícano", lugar: "Caleta Portales, Valparaíso", fecha: "04/09/2026", hora: "12:10" },
+    { tipo: "Ave corredora", nombre: "Ñandú", lugar: "Torres del Paine", fecha: "25/08/2026", hora: "15:45" },
+    { tipo: "Picaflor", nombre: "Picaflor de Arica", lugar: "Valle de Azapa", fecha: "02/09/2026", hora: "16:00" }
 ];
 
 const cargaLista = () => {
@@ -20,4 +29,34 @@ const cargaLista = () => {
     });
 };
 
-window.onload = cargaLista;
+const ordenarLista = () => {
+    const campo = document.getElementById("filtro-campo").value;
+    const orden = document.getElementById("filtro-orden").value;    
+
+    bdAvistamientos.sort((a,b) =>{
+        let valorA = a[campo];
+        let valorB = b[campo];
+
+        if (campo == "fecha") {
+            const [diaA, mesA, anoA] = valorA.split("/");
+            const [diaB, mesB, anoB] = valorB.split("/");
+            valorA = new Date(`${anoA}-${mesA}-${diaA}`);
+            valorB = new Date(`${anoB}-${mesB}-${diaB}`);
+            return orden == "asc" ? valorA - valorB : valorB - valorA;
+        } 
+        
+        if (orden == "asc") {
+            return valorA.localeCompare(valorB);
+        } else {
+            return valorB.localeCompare(valorA);
+        }
+    });
+    cargaLista();
+};
+
+document.getElementById("filtro-campo").addEventListener("change", ordenarLista);
+document.getElementById("filtro-orden").addEventListener("change", ordenarLista);
+
+window.onload = () => {
+    ordenarLista();
+};
